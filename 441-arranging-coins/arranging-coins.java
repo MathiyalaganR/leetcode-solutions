322a1e544ee82c9eabf4d47f0e815033ba3532ae
MathiyalaganR <158831742+MathiyalaganR@users.noreply.github.com>
2025-01-28 07:17:45 +0530

    Time: 1 ms (98.50%) | Memory: 41 MB (54.57%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,23 +1,23 @@
 class Solution {
     public int arrangeCoins(int n) {
-        // Initialize variables
+        
         int left = 0, right = n;
 
-        // Binary search to find the largest k such that k * (k + 1) / 2 <= n
+        
         while (left <= right) {
-            int mid = left + (right - left) / 2;  // Middle value to check the sum of first 'mid' rows
-            long sum = (long)mid * (mid + 1) / 2;  // Sum of first 'mid' rows
+            int mid = left + (right - left) / 2;  
+            long sum = (long)mid * (mid + 1) / 2;
             
-            if (sum == n) {  // Exact match
+            if (sum == n) {  
                 return mid;
-            } else if (sum < n) {  // If sum is less than n, we can try for more rows
+            } else if (sum < n) { 
                 left = mid + 1;
-            } else {  // If sum is greater than n, we need fewer rows
+            } else {  
                 right = mid - 1;
             }
         }
         
-        // If no exact match, return 'right', which gives the largest valid number of rows
+        
         return right;
     }
 }
